54a3a167836fcbf19f956e7096742ee62fa1c7a5
DiegoSaravia6 <diego.saravia@correo.ucu.edu.uy>
2025-04-30 20:33:49 -0300

    Condicionales y tests finales

--- src/Library/DateFormatter.cs
+++ src/Library/DateFormatter.cs
@@ -1,5 +1,6 @@
 ﻿namespace Ucu.Poo.TestDateFormat;
 
+
 /// <summary>
 /// Esta clase implementa la funcionalidad de cambiar el formato de una fecha.
 /// </summary>
@@ -16,17 +17,41 @@ public class DateFormatter
     public static string ChangeFormat(string date)
     {
         if(string.IsNullOrEmpty(date)){
-            return "Error: Blanc date";
+            return "Error: Invalid date";
         }
         if(!(date[2].Equals('/') && date[5].Equals('/'))){
-            return "Error: Wrong format";
+            return "Error: Invalid format";
         }
         foreach (char c in (date.Substring(0,2)+date.Substring(3,2)+date.Substring(6)))
         {
             if(!char.IsDigit(c)){
-                return "Error: Wrong format";
+                return "Error: Invalid date";
             }
         }
+        
+        string stringDays = date.Substring(0, 2);
+        int days = Convert.ToInt32(stringDays);
+        if(days>31)
+        {
+            return "Error: Invalid day";
+        }
+        string stringMonths = date.Substring(3, 2);
+        int months = Convert.ToInt32(stringMonths);
+        if(months>12)
+        {
+            return "Error: Invalid month";
+        }
+        if (months == 1 || months == 3 || months == 5 || months == 7 || months == 8 || months == 10 || months == 12 && days>31)
+        {
+            return "Error: Invalid date";
+        }
+        
+        if ((months == 2 && days>29) || (months == 4 || months == 6 || months == 9 || months == 11 && days>30))
+        {
+            return "Error: Invalid date";
+        }
+        
+        
         return date.Substring(6) + "-" + date.Substring(3, 2) + "-" + date.Substring(0, 2);
     }
 }
--- tests/LibraryTests/DataFormatterTests.cs
+++ tests/LibraryTests/DataFormatterTests.cs
@@ -15,7 +15,7 @@ public class DateFormatterTests
     public void TestRightFormat()
     {
         string testDate = "12/11/1999";
-        string expectedResult =  "1999-11-12";
+        string expectedResult = "1999-11-12";
         string result = DateFormatter.ChangeFormat(testDate) ;
         
         Assert.That(result == expectedResult,$"expected: {expectedResult} \n    result: {result}");
@@ -24,7 +24,7 @@ public class DateFormatterTests
     public void TestWrongFormat()
     {
         string testDate = "12/11199";
-        string expectedResult = "Error: Wrong format";
+        string expectedResult = "Error: Invalid format";
         string result = DateFormatter.ChangeFormat(testDate) ;
         
         Assert.That(result == expectedResult,$"expected: {expectedResult} \n    result: {result}");
@@ -33,10 +33,48 @@ public class DateFormatterTests
     public void TestBlancDate()
     {
         string testDate = "";
-        string expectedResult = "Error: Blanc date";
+        string expectedResult = "Error: Invalid date";
+        string result = DateFormatter.ChangeFormat(testDate) ;
+        
+        Assert.That(result == expectedResult,$"expected: {expectedResult} \n    result: {result}");
+    }
+    [Test]
+    public void TestDigit()
+    {
+        string testDate = "3A/10/2020";
+        string expectedResult = "Error: Invalid date";
         string result = DateFormatter.ChangeFormat(testDate) ;
         
         Assert.That(result == expectedResult,$"expected: {expectedResult} \n    result: {result}");
     }
 
+    [Test]
+    public void TestDays()
+    {
+        string testDate = "50/12/2002";
+        string expectedResult = "Error: Invalid day";
+        string result = DateFormatter.ChangeFormat(testDate) ;
+        
+        Assert.That(result == expectedResult,$"expected: {expectedResult} \n    result: {result}");
+    }
+
+    [Test]
+    public void TestMonths()
+    {
+        string testDate = "20/92/1902";
+        string expectedResult = "Error: Invalid month";
+        string result = DateFormatter.ChangeFormat(testDate) ;
+        
+        Assert.That(result == expectedResult,$"expected: {expectedResult} \n    result: {result}");
+    }
+
+    [Test]
+    public void TestFebrero()
+    {
+        string testDate = "31/02/2002";
+        string expectedResult = "Error: Invalid date";
+        string result = DateFormatter.ChangeFormat(testDate) ;
+        
+        Assert.That(result == expectedResult,$"expected: {expectedResult} \n    result: {result}");
+    }
 }
